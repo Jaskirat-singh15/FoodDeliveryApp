a27daa2fb6af3295534c73063dd7888073bbea72
MehkeetSingh <mehkeetsingh17@gmail.com>
2023-05-31 21:51:09 +0530

    done till 8:17:20

--- client/src/animations/index.js
+++ client/src/animations/index.js
@@ -8,8 +8,18 @@ export const fadeInOut = {
   exit: { opacity: 0 },
 };
 
-export const slideTop={
-  initial:{opacity: 0,y: 30}, 
-  animate:{opacity: 1,y: 0},
-  exit:{opacity:0,y:30},
+export const slideTop = {
+  initial: { opacity: 0, y: 30 },
+  animate: { opacity: 1, y: 0 },
+  exit: { opacity: 0, y: 30 },
 }
+
+export const staggerFadeInOut = (i) => {
+  return {
+    initial: { opacity: 0, y: 50 },
+    animate: { opacity: 1, y: 0 },
+    exit: { opacity: 0, y: 50 },
+    transition: { duration: 0.3, delay: i * 0.15 },
+    key: { i },
+  };
+};
