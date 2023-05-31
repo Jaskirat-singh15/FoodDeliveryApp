bd767df344e1c59b9df91aef9ab03a54d7058dcf
ArpitBirSingh <arpitbirsingh65@gmail.com>
2023-05-31 11:34:47 +0530

    fix errors

--- client/src/components/DBHome.jsx
+++ client/src/components/DBHome.jsx
@@ -1,11 +1,20 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { getAllProducts } from '../api';
+import { getAllProducts, getAllUsers } from '../api';
 import { setAllProducts } from '../context/actions/productActions';
+import { setAllUserDetails } from '../context/actions/allUsersAction';
 
 const DBHome = () => {
   const products = useSelector((state) => state.products);
   const dispatch = useDispatch();
+
+  const allUsers = useSelector((state)=> state.allUsers);
+
+  useEffect(() => {
+    if(!allUsers) {
+      getAllUsers().then((data)=>{ dispatch(setAllUserDetails(data)); });
+    }
+  }, []);
 
   useEffect(() => {
     if(!products) {
