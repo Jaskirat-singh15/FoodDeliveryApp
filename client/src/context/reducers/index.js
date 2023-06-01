c30fdb84bb3a73008ee280bd3e72a6dfc8751fe4
MehkeetSingh <mehkeetsingh17@gmail.com>
2023-06-01 15:49:21 +0530

    client screen UI done

--- client/src/context/reducers/index.js
+++ client/src/context/reducers/index.js
@@ -4,11 +4,13 @@ import alertReducer from "./alertReducer";
 import productReducer from "./productReducer";
 import allUserReducer from "./allUserReducer";
 import cartReducer from "./cartReducer";
+import displayCartReducer from "./displayCartReducer";
 const myReducers = combineReducers({
   user: userReducer,
   alert: alertReducer,
   products: productReducer,
   allUsers: allUserReducer,
   cart: cartReducer,
+  isCart: displayCartReducer,
 });
 export default myReducers;
